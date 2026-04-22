52d68f4ef1eb56388e1467f2bea99810b38526ef
ara1konn <baidjan.arailym@gmail.com>
2026-04-22 23:49:36 +0600

    важные правки

--- database.py
+++ database.py
@@ -1,7 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:ara1kon@localhost:5432/furniture_db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
